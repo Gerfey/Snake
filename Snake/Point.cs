dc7cf4a02f638db9fb54c6efe06dae557f8e7240
Gerfey <sanjia@live.ru>
2018-12-28 11:31:51 +0700

    Завершили работу над змейкой

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -19,10 +19,52 @@ namespace Snake
             this.sym = _sym;
         }
 
+        public Point(Point p)
+        {
+            this.x = p.x;
+            this.y = p.y;
+            this.sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.UP:
+                    y += offset;
+                    break;
+                case Direction.DOWN:
+                    y -= offset;
+                    break;
+                case Direction.LEFT:
+                    x -= offset;
+                    break;
+                case Direction.RIGHT:
+                    x += offset;
+                    break;
+            }
+        }
+
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
         }
     }    
 }
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -9,13 +10,68 @@ namespace Snake
     class Program
     {
         static void Main(string[] args)
-        {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-            
-            Console.ReadKey();
+        {   
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
+            {
+                if(walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(snake.GetSpeed());
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }                
+            }
+
+            Console.Clear();
+            WriteGameOver(snake.GetEat());
+            Console.ReadLine();
         }
 
-        
+        static void WriteGameOver(int EatCount)
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition( xOffset, yOffset++ );
+			WriteText( "============================", xOffset, yOffset++ );
+			WriteText( "И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++ );
+			yOffset++;
+			WriteText( "Вы достигли ускорения: " + (EatCount * 10), xOffset + 2, yOffset++ );
+			WriteText( "Змейка поглотила " + EatCount + " ед. еды.", xOffset + 1, yOffset++ );
+			WriteText( "============================", xOffset, yOffset++ );
+		}
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
+        }
+
+
     }
 }
